889692dbcc21b3b92bb5e5f3da7de70ba2c543e1
Poojan Ghetiya <poojanghetiya29@gmail.com>
2024-08-20 21:40:17 +0530

    Secret key hidden

--- a_core/settings.py
+++ a_core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n&$#appsr=k)4!xx*o_7q@a9_i5jp17w5haojlw^wrx7tnout9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
